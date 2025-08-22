52d91aa8d5021af87debc0877351cccea083b694
kosumi <eugeneq2632@gmail.com>
2025-08-22 11:41:14 +0800

    Added some sample code

---
+++ yo.php
@@ -0,0 +1,13 @@
+<?php
+// Sample PHP code
+
+echo "Hello, WaifuLand!";
+
+// Define a simple function
+function greet($name) {
+    return "Hello, " . htmlspecialchars($name) . "!";
+}
+
+// Example usage
+echo greet("User");
+?>
