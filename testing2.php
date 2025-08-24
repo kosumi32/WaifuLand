7cc8f6630c40c537aafb1d20dc82afdcd27d492a
kosumi <eugeneq2632@gmail.com>
2025-08-24 23:51:37 +0800

    test2

---
+++ testing2.php
@@ -0,0 +1,10 @@
+<?php
+// Generate a random number between 1 and 100
+$randomNumber = rand(1, 100);
+
+// Generate a random string of 8 characters
+$randomString = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 8);
+
+echo "Random Number: $randomNumber\n";
+echo "Random String: $randomString\n";
+?>
